8c6f0d1d4420c0f081d953a5fb477b9e40e85ca6
WahyuS002 <syahputrawahyu61@gmail.com>
2021-12-18 14:26:10 +0700

    :white_check_mark: Minting Section

--- src/views/Index.js
+++ src/views/Index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Main from '../components/Main'
 import Navbar from '../components/Navbar'
+import MintingSection from '../components/MintingSection'
+import Footer from '../components/Footer'
 
 function Index() {
     return (
@@ -9,6 +11,16 @@ function Index() {
                 <Navbar />
                 <div className="mt-12 md:mt-28">
                     <Main />
+                </div>
+            </div>
+            <div className="bg-white">
+                <div className="mt-12 md:mt-28">
+                    <MintingSection />
+                </div>
+            </div>
+            <div className="bg-white pb-2 md:pb-16">
+                <div className="mt-12 md:mt-28">
+                    <Footer />
                 </div>
             </div>
         </div>
