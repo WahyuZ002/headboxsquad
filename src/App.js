8ba4b7b1c08230e3b9f93ffa4c6989b6d097dbfd
WahyuS002 <syahputrawahyu61@gmail.com>
2021-12-18 09:23:21 +0700

    :zap: quick bug fix

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import Index from './views/Index'
 
 function App() {
     return (
-        <div className="text-dark bg-off-white">
-            <div className="py-2 md:py-16">
-                <Index />
-            </div>
+        <div className="text-dark">
+            <Index />
         </div>
     )
 }
--- src/views/Index.js
+++ src/views/Index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Main from '../components/Main'
 import Navbar from '../components/Navbar'
 
-function Index(props) {
+function Index() {
     return (
         <div className="font-open-sans text-dark">
-            <Navbar />
-            <div className="mt-12 md:mt-28">
-                <Main />
+            <div className="bg-off-white pt-2 md:pt-16">
+                <Navbar />
+                <div className="mt-12 md:mt-28">
+                    <Main />
+                </div>
             </div>
         </div>
     )
