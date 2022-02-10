3969a0529e1530588aaa00b7838b0cc73460fdf0
WahyuS002 <syahputrawahyu61@gmail.com>
2022-02-10 15:25:38 +0700

    :zap: maintenance true

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Router from './router'
 import Maintenance from './components/Maintenance'
 
 function App() {
-    const maintenance = false
+    const maintenance = true
     return <div className="text-dark">{maintenance ? <Maintenance /> : <Router />}</div>
 }
 
